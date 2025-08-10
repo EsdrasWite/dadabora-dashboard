5fbfb2d01caa0ac5d6a9f99cb92cdc6b07c6dcf5
esdraswite <witeesdras@gmail.com>
2025-08-11 01:16:47 +0200

    routes architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-function App() {
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/not-found/NoteFound";
+import Dashboard from "./pages/dashboard/Dashboard";
+
+const HomeLayout = () => {
   return (
     <>
-      <h1 className="text-3xl text-teal-900"> hello word</h1>
+      <div className="flex h-screen bg-gray-50">
+        {/* <SideBar /> */}
+        {/* <main id="main" className="main dashboard"> */}
+        <Outlet />
+        {/* </main> */}
+        {/* <Footer />
+      <BackToTop /> */}
+      </div>
     </>
   );
+};
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        { path: "/", element: <Dashboard /> },
+
+        {
+          path: "/login",
+          element: <h1>Login</h1>,
+        },
+        {
+          path: "/aide",
+          element: <h1>Aide</h1>,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
