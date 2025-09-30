7dc5b166dadd055f180aefbb115d6645573134b2
esdraswite <witeesdras@gmail.com>
2025-09-30 13:33:02 +0200

    ad logo

--- src/pages/not-found/NoteFound.tsx
+++ src/pages/not-found/NoteFound.tsx
@@ -1,39 +1,34 @@
 import { Link } from "react-router-dom";
-// import logoImg from "../../images/logo.png";
+import logo from "../../assets/logo.jpg";
 
 function NotFound() {
   return (
-    <>
-      <header
-        id="header"
-        className="header fixed-top d-flex align-items-center px-4 py-3 bg-white shadow"
-      >
-        {/* logo */}
-        <div className="d-flex align-items-center justify-content-between">
-          <Link to="/" className="logo d-flex align-items-center">
-            {/* <img src={logoImg} alt="" /> */}
-            <span className="d-none d-lg-block">Dada Bora</span>
-          </Link>
+    <div className="max-h-screen flex flex-col min-h-screen">
+      <header className="w-full px-6 py-4 bg-slate-50 border-b border-solid border-gray-200">
+        <div className="max-w-7xl mx-auto flex justify-between items-center">
+          <div className="flex items-center space-x-2">
+            <img src={logo} alt="Logo" width={80} className="fit-content" />
+          </div>
         </div>
       </header>
       {/* Not Found Page Content */}
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-1 items-center justify-center bg-gray-100">
         <div className="text-center">
-          <h1 className="text-6xl font-bold text-[#012970]">404</h1>
-          <p className="mt-4 text-2xl text-[#012970]">Page non trouvéé</p>
+          <h1 className="text-8xl font-bold text-pink-500">404</h1>
+          <p className="mt-4 text-2xl text-[#323232]">Page non trouvéé</p>
           <p className="mt-2 text-gray-800">
             La page que vous recherchez n'existe pas ou a été déplacée.
           </p>
           <a
-            href="/"
-            className="mt-6 inline-block px-4 py-3 bg-[#012970] text-white rounded hover:bg-blue-800 transition duration-300"
+            href="/login"
+            className="mt-6 inline-block px-4 py-3 bg-pink-500 hover:bg-pink-600 text-white rounded transition duration-300"
           >
-            {/* <CircleArrowLeft size={20} className="inline mr-2" /> */}
+            <i className="fas fa-arrow-left mr-2"></i>
             Go Back Home
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
